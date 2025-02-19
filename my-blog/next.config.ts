4199856cc5ea9f014932b322f42bd433e5562182
s.han <s.han@morph.kr>
2025-02-19 18:18:29 +0900

    [fix] next config basePath 조건부 추가
    배포 환경에서만 basePath가 적용되도록 수정

--- my-blog/next.config.ts
+++ my-blog/next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'build',
-  basePath: '/blog-front-end',
+  basePath: isProduction ? '/blog-front-end': '',
 };
 
 export default nextConfig;
