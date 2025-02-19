c79f5cbc3955f2aa67f9d2801f4096431aa0945c
s.han <s.han@morph.kr>
2025-02-19 11:27:39 +0900

    [fix] basePath 최상단으로 수정

--- my-blog/next.config.ts
+++ my-blog/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'build',
-  basePath: '/my-blog',
+  basePath: '/blog-front-end',
 };
 
 export default nextConfig;
