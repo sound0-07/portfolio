935476af1971424865700ae4216bf742d82fd127
s.han <s.han@morph.kr>
2025-02-20 19:00:48 +0900

    [add] page router test, metadata 수정

---
+++ my-blog/src/app/main/page.tsx
@@ -0,0 +1,15 @@
+export default function Main() {
+    const content = "HHHHHHHH";
+    const name = "sound0-07";
+
+    return (
+        <div>
+            <main>
+                <p>{content}</p>
+            </main>
+            <footer>
+                <p>{name}</p>
+            </footer>
+        </div>
+    );
+}
