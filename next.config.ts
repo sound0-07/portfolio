efb6adae53020b1988711772984a74941a2123f7
sound <hansol13084@gmail.com>
2025-06-27 15:49:18 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
 	output: 'export',
-	basePath: process.env.NODE_ENV === 'production' ? '/blog-front-end' : '',
+	basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
 };
 
 export default nextConfig;
